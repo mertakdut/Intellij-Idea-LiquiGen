36be0ad2d6799a143de4a5065baaf18313690d26
GitHub Action <action@github.com>
2021-03-22 13:03:49 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mertakdut/intellijidealiquigen/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mertakdut.intellijidealiquigen.services
+
+import com.github.mertakdut.intellijidealiquigen.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mertakdut/intellijidealiquigen/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mertakdut/intellijidealiquigen/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mertakdut.intellijidealiquigen.services
 
+import com.github.mertakdut.intellijidealiquigen.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
